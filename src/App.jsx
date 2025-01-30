8c2691189c7bd98a069778a6a66c27bb66c3f64f
chris <christopher.peters@itpeoplecorp.com>
2025-01-30 23:36:24 +0530

    first react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ padding: "20px", margin: "20px", backgroundColor: "skyblue" }}>
+        <h1>Hello React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

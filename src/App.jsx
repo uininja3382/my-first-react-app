9920e9c9ad576c01df777d7ba60f2e40827e3ce4
chris <christopher.peters@itpeoplecorp.com>
2025-01-31 23:46:08 +0530

    React state and setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import { Component } from "react";
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Daniel",
+      position: "UI developer",
+      bgColor: "green",
+    };
+  }
+
   render() {
     return (
-      <div style={{ padding: "20px", margin: "20px", backgroundColor: "skyblue" }}>
-        <h1>Hello React</h1>
+      <div
+        style={{
+          padding: "20px",
+          margin: "20px",
+          backgroundColor: `${this.state.bgColor}`,
+        }}
+      >
+        <h1>
+          Hello am {this.state.name} my position is {this.state.position}
+        </h1>
+        <button
+          onClick={() => {
+            this.setState({
+              name: "Madhu",
+              position: "Software Developer",
+              bgColor: "yellow",
+            });
+          }}
+        >
+          Change Name
+        </button>
       </div>
     );
   }
